33be6747520404f94e8b745eb49e4e02bf29c6bd
howooking <junsgk@gmail.com>
2023-06-29 16:46:21 +0900

    feat: Navbar수정, Footer 생성
    - react router의 <Link>태그로 페이지 이동 구현
    - 메인로고 이미지 교체

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,29 @@
 import { NAV_ITEMS } from '@/constants/constants';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <header className="container mx-auto flex items-center justify-between px-10 py-5">
       <div className="flex items-center gap-10">
-        <img src="/popple.jpg" alt="main-logo" className="h-8" />
+        <Link to="/">
+          <img src="/popple.jpg" alt="main-logo" className="h-8" />
+        </Link>
         <ul className="flex gap-3">
           {NAV_ITEMS.map((item) => (
-            <li key={item.label}>{item.label}</li>
+            <li key={item.label}>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
           ))}
         </ul>
       </div>
       <div>
         <ul className="flex gap-2">
-          <li className="text-gray-400">회원가입</li>
-          <li>로그인</li>
+          <li>
+            <Link to="/login">로그인</Link>
+          </li>
+          <li>
+            <Link to="/signup">회원가입</Link>
+          </li>
         </ul>
       </div>
     </header>
