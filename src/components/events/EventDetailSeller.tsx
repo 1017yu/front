1cc576d1c1164b8ae726d60fc453d842fc29a031
Kled <1017yu@daum.net>
2023-07-27 21:54:14 +0900

    feat: 참여중인 셀러 컴포넌트 임시 생성

---
+++ src/components/events/EventDetailSeller.tsx
@@ -0,0 +1,7 @@
+export default function EventDetailSeller() {
+  return (
+    <div className="mb-12 flex justify-center bg-slate-300 sm:min-h-[30rem] sm:px-16 sm:py-16">
+      <div className="sm:text-4xl">참여하는 셀러</div>
+    </div>
+  );
+}
