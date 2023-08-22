c27d9cab295bf5c204f07f49fb984cbc3c313eb2
Kled <1017yu@daum.net>
2023-08-22 22:49:30 +0900

    fix: 파일 경로 수정

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <div className="bg-black">
       <img
-        src="/login.jpg"
+        src="/src/assets/login.jpg"
         alt="banner"
         className="mx-auto h-96 object-cover md:w-full"
       />
