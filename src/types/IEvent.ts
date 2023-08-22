07c701b35f8c7dcf8cdfe7b9615b68263d30c39d
Kled <1017yu@daum.net>
2023-08-22 22:53:42 +0900

    fix: 모든 이벤트 조회 인터페이스 수정

--- src/types/IEvent.ts
+++ src/types/IEvent.ts
@@ -1,9 +1,15 @@
+// 상세 이벤트 조회 Response interface
+export interface IEventResponse<T> {
+  data: T;
+}
+
 export interface IEvent {
-  id?: number;
+  id?: string;
   thumbnailUrl?: string;
   name?: string;
   hostName?: string;
-  location?: string;
+  city?: string;
+  district?: string;
   description?: string;
   category?: string;
   startDate?: string;
@@ -11,19 +17,11 @@ export interface IEvent {
   createdAt?: string;
   updatedAt?: string;
   status?: string; // 진행중/마감
+  isOwner?: boolean;
+  isParticipant?: boolean;
 }
 
-export const dummyEventData: IEvent = {
-  id: 1,
-  thumbnailUrl: 'https://storage.googleapis.com/2',
-  name: '팝업 스토어',
-  hostName: '유희태', // 개인판매자 이름
-  location: '서울 강남구 서초동',
-  description: '가나다라마바사',
-  category: '레저/스포츠',
-  startDate: '2021-10-29T07:20:02.749Z',
-  endDate: '2021-10-29T07:20:02.749Z',
-  createdAt: '2021-10-29T07:20:02.749Z',
-  updatedAt: '2021-10-29T07:20:02.749Z',
-  status: '진행중', // 진행중/마감
-};
+export interface IEventJoinProps {
+  isOwner?: boolean;
+  isParticipant?: boolean;
+}
