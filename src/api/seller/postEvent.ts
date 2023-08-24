e11b71098e68d8fe52e60798bfcc756e993ebf64
EungBug <eungb14@gmail.com>
2023-08-24 21:50:04 +0900

    feat: 이미지 AWS S3 Upload 추가

--- src/api/seller/postEvent.ts
+++ src/api/seller/postEvent.ts
@@ -1,12 +1,12 @@
 import { apiInstance } from '@/api/axios';
-import { SELLER_ACCESS_TOKEN } from '@/data/test';
+// import { SELLER_ACCESS_TOKEN } from '@/data/test';
 import { TPostEventType } from '@/types/TPostEventType';
 
 // * POST 셀러의 이벤트 등록
 export const postEvent = async (postEventData: TPostEventType) => {
   const response = await apiInstance.post('/events', postEventData, {
     headers: {
-      Authorization: `Bearer ${SELLER_ACCESS_TOKEN}`,
+      // Authorization: `Bearer ${SELLER_ACCESS_TOKEN}`,
     },
   });
   return response.data;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,12 +6,17 @@ import { ISurveyResponse } from '@/types/ISurvey';
 import { useUser } from '@/hooks/useUser';
 import moment from 'moment';
 
+import ReactS3Client from 'react-aws-s3-typescript';
+import { boardConfig } from '@/data/s3configs';
+
 export default function Home() {
   const [activeSurvey, setActiveSurvey] = useState<ISurveyResponse | null>(
     null,
   );
   const closeTodayDate = localStorage.getItem('CloseTodayDate');
   const { user } = useUser();
+
+  const [imageFile, setImageFile] = useState<File | null>(null);
 
   useEffect(() => {
     fetchActiveSurvey().then((res) => {
@@ -23,11 +28,49 @@ export default function Home() {
     setActiveSurvey(null);
   }, []);
 
+  const uploadImage = async (file: File) => {
+    const s3 = new ReactS3Client(boardConfig);
+    try {
+      const fileName = `${moment().format('YYMMDDhh:mm:ss')}_${
+        file.name.split('.')[0]
+      }`;
+      const res = await s3.uploadFile(file, fileName);
+      setImageFile(null);
+      console.log(res.location);
+    } catch (error) {
+      // TODO : 파일 업로드 실패 예외처리
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile !== null) {
+      uploadImage(imageFile);
+    }
+  }, [imageFile]);
+
+  const onChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      if (file.size > 5000000) {
+        // TODO 파일 사이즈 제한 예외처리
+        return;
+      }
+
+      setImageFile(file);
+    }
+  };
+
   return (
     <Container>
       <div>이메일 : {user?.email}</div>
       <div>닉네임 : {user?.nickname}</div>
       <div>프로필이미지url : {user?.profileImgUrl}</div>
+      {/* 이미지 업로드 S3 테스트 */}
+      <form>
+        <input type="file" accept="image/*" onChange={onChangeFile} />
+      </form>
+
       {/* {user?.accessToken} */}
       {activeSurvey &&
         !activeSurvey.isDone &&
