fdc92c7fa3bf58d6debc9394421e6a8cfcbe0039
Kled <1017yu@daum.net>
2023-08-25 13:27:07 +0900

    fix: env 재설정

--- .env.d.ts
+++ .env.d.ts
@@ -1,6 +1,6 @@
 interface ImportMeta {
   env: {
-    VITE_MAP_BASE_URL?: string;
-    VITE_MAP_API_KEY?: string;
+    VITE_KAKAO_MAP_BASE_URL?: string;
+    VITE_KAKAO_MAP_API_KEY?: string;
   };
 }
