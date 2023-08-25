0e0a10b7dca8d20961f86c2db3c0313e0cc0f6be
Kled <1017yu@daum.net>
2023-08-26 00:32:05 +0900

    feat: event constant 수정

--- src/components/events/EventDetailDesc.tsx
+++ src/components/events/EventDetailDesc.tsx
@@ -1,19 +1,26 @@
 import EventMap from './EventMap';
+import Hr from '@/components/ui/Hr';
 import { IEvent } from '@/types/IEvent';
-import { EVENTS_DESCRIPTION } from '@/data/constants';
-import EventDetailTitle from '@/components/events/EventDetailTitle';
+import { eventData } from '@/data/constants';
+import EventTitle from '@/components/events/EventTitle';
 
 export default function EventDetailDesc({ description }: IEvent) {
   return (
     <>
-      <EventDetailTitle title={EVENTS_DESCRIPTION.discription} />
-      <div className="m-4 min-h-[6rem] rounded-sm text-center text-lg text-gray-600 sm:m-4 sm:min-h-[10rem]">
-        {description}
+      <div className="mb-4 mt-8 sm:mt-16">
+        <EventTitle title={eventData.EVENT_DETAIL_DESCRIPTION.title} center />
+        <div className="mt-2 min-h-[6rem] rounded-sm text-center text-lg text-gray-600 sm:mt-4 sm:min-h-[10rem]">
+          {description}
+        </div>
+        <Hr />
       </div>
 
-      <EventDetailTitle title={EVENTS_DESCRIPTION.map} />
-      <div className="flex justify-center">
-        <EventMap />
+      <div className="mb-4 mt-8 hidden sm:mt-16 sm:block">
+        <EventTitle title={eventData.EVENT_DETAIL_MAP.title} center />
+        <div className="mt-4 flex justify-center">
+          <EventMap />
+        </div>
+        <Hr />
       </div>
     </>
   );
--- src/components/seller/PostTitle.tsx
+++ src/components/seller/PostTitle.tsx
@@ -1,9 +1,9 @@
-import { POST_TITLE } from '@/data/constants';
+import { eventData } from '@/data/constants';
 
 export default function PostTitle() {
   return (
     <div className="mt-16 text-center text-2xl font-semibold sm:text-left sm:text-3xl">
-      {POST_TITLE}
+      {eventData.EVENT_POST_TITLE}
     </div>
   );
 }
