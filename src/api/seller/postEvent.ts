91df4e4824f13ccf86b3834dac9fd954cc20ec8b
Kled <1017yu@daum.net>
2023-08-25 11:44:11 +0900

    feat: 셀러의 이벤트 등록 api 세팅

--- src/api/seller/postEvent.ts
+++ src/api/seller/postEvent.ts
@@ -4,11 +4,14 @@ import { TPostEventType } from '@/types/TPostEventType';
 // * POST 셀러의 이벤트 등록
 export const postEvent = async (postEventData: TPostEventType) => {
   const getUser = localStorage.getItem('user');
-  const accessToken = JSON.parse(getUser as string).accessToken;
-  const response = await apiInstance.post('/events', postEventData, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return response.data;
+  const accessToken = getUser ? JSON.parse(getUser).accessToken : null;
+
+  if (accessToken) {
+    const response = await apiInstance.post('/events', postEventData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  }
 };
