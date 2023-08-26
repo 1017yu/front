416eab4acfe9aca119fff62f4c6ee1042a4c9e49
Kled <1017yu@daum.net>
2023-08-26 17:58:10 +0900

    feat: 모든 이벤트 조회 페이징 마이그레이션

--- src/components/events/EventList.tsx
+++ src/components/events/EventList.tsx
@@ -1,18 +1,14 @@
 import Title from '@/components/ui/Title';
 import Button from '@/components/ui/Button';
 import { eventData } from '@/data/constants';
+import { IBookmark } from '@/types/IBookmark';
 import { fetchEvents } from '@/api/events/events';
-import Pagination from '@mui/material/Pagination';
+import EventLayout from './EventLayout';
 import { useEffect, useMemo, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import EventLayout from '@/components/events/EventLayout';
+import { totalEventsState, searchOptionState } from '@/states/Events';
+import PaginationComponent from '@/components/community/Pagination';
 import { IEvents, IEventsPagination } from '@/types/IEvents';
-import { numberOfEventState, searchOptionState } from '@/states/Events';
-import {
-  COUNT_PER_EVENTS_PAGE,
-  EVENTS_THEME,
-  eventData,
-} from '@/data/constants';
 
 export default function EventList() {
   const [page, setPage] = useState(1); // 페이지 번호
@@ -21,6 +17,7 @@ export default function EventList() {
   const [isSeller, setIsSeller] = useState<boolean>(false); // 일반 유저 or 셀러
   const [eventsList, setEventsList] = useState<IEvents[]>([]); // 모든 이벤트 목록
   const [totalEvents, setTotalEvents] = useRecoilState(totalEventsState); // 등록된 모든 이벤트의 개수
+const [localBookmarked, setLocalBookmarked] = useState<IBookmark[]>([]);
 
   // 공고 등록 페이지로 이동
   const handleMovePostEvent = () => {
@@ -46,13 +43,19 @@ export default function EventList() {
 
     // 로컬스토리지에서 유저 role get
     const getUserRole = localStorage.getItem('user');
+    const getBookmark = localStorage.getItem('bookmark');
 
     // 로컬 스토리지에 user 값이 존재할 때
     if (getUserRole) {
       const userRole = JSON.parse(getUserRole).role;
+      
 
       // userRole이 셀러일 때만 button render
       userRole === 'ROLE_SELLER' ? setIsSeller(true) : setIsSeller(false);
+
+      if (getBookmark) {
+        setLocalBookmarked(JSON.parse(getBookmark));
+      }
     }
   }, [setTotalEvents, totalEvents]);
 
@@ -68,71 +71,45 @@ export default function EventList() {
     }
 
     // 검색 조건에 따른 이벤트 목록 조회
-    return eventsList.filter((event) => {
-      const cityMatch =
-        searchOption.city === '' || event.city === searchOption.city;
-      const districtMatch =
-        searchOption.district === '' ||
-        event.district === searchOption.district;
-      const categoryMatch =
-        searchOption.category === '' ||
-        event.category === searchOption.category;
+    const searchedList = useMemo(() => {
+    const { city, district, category } = searchOption;
 
-      return (
-        cityMatch &&
-        (districtMatch || searchOption.district === '') &&
-        categoryMatch
-      );
-    });
-  }, [eventsList, searchOption]);
+    return (
+      eventsList
+        // 1. 검색 옵션 여부에 따라 필터링
+        .filter((event) => {
+          const cityMatch = city === '' || event.city === city;
+          const districtMatch = district === '' || event.district === district;
+          const categoryMatch = category === '' || event.category === category;
+
+          return cityMatch && districtMatch && categoryMatch;
+        })
+        // 2. 필터링 된 이벤트 중 북마크 여부 -> 로컬 스토리지에 저장된 id값으로 비교
+        .map((event) => ({
+          ...event,
+          bookmark: localBookmarked.some(
+            (value) => value.id === event.id && value.bookmark,
+          ),
+        }))
+        // 3. 최근 등록된 이벤트 순으로 정렬
+        .sort((a, b) => b.id - a.id)
+    );
+  }, [eventsList, localBookmarked, searchOption]);
 
   return (
-    <ThemeProvider theme={EVENTS_THEME}>
-      <div className="container mx-auto px-8 sm:px-20">
-        <div className="flex items-center justify-between">
-          <Title text={eventData.EVENT_LIST_TITLE} />
-          {isSeller ? (
-            <div className="sm:min-w-[12rem]">
-              <Button onClick={handleMovePostEvent} contents={'공고 등록'} />
-            </div>
-          ) : (
-            ''
-          )}
-        </div>
-        <div className="container mx-auto mt-8 sm:mt-16">
-          <div className="flex flex-wrap justify-between">
-            {searchedList.map((event) => (
-              <EventLayout
-                key={event.id}
-                id={event.id}
-                name={event.name}
-                city={event.city}
-                district={event.district}
-                thumbnailUrl={event?.thumbnailUrl}
-                category={event?.category}
-                status={event.status}
-                bookmark={event.bookmark}
-              />
-            ))}
+    <div className="container mx-auto px-8 sm:px-20">
+      <div className="flex items-center justify-between">
+        <Title text={eventData.EVENT_LIST_TITLE} />
+        {isSeller && (
+          <div className="sm:min-w-[2rem]">
+            <Button onClick={handleMovePostEvent} contents={'공고 등록'} />
           </div>
-        ) : (
-          ''
         )}
       </div>
       <div className="container mx-auto mt-8 sm:mt-16">
         <div className="flex flex-wrap justify-between">
           {searchedList.map((event) => (
-            <EventLayout
-              key={event.id}
-              id={event.id}
-              name={event.name}
-              city={event.city}
-              district={event.district}
-              thumbnailUrl={event?.thumbnailUrl}
-              category={event?.category}
-              status={event.status}
-              bookmark={event.bookmark}
-            />
+            <EventLayout key={event.id} {...event} />
           ))}
         </div>
       </div>
