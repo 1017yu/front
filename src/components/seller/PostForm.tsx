1d7ae33984b092c89674db6296846da4d6ed537b
Kled <1017yu@daum.net>
2023-08-26 16:46:29 +0900

    feat: 스토어 등록 및 수정 유효성 검사

--- src/components/seller/PostForm.tsx
+++ src/components/seller/PostForm.tsx
@@ -1,39 +1,53 @@
 import { useRecoilValue } from 'recoil';
+import Title from '@/components/ui/Title';
 import Button from '@/components/ui/Button';
+import { eventData } from '@/data/constants';
 import customToast from '@/utils/customToast';
 import { useNavigate } from 'react-router-dom';
+import { eventFormState } from '@/states/Events';
 import { postEvent } from '@/api/seller/postEvent';
 import PostDesc from '@/components/seller/PostDesc';
-import PostTitle from '@/components/seller/PostTitle';
 import PostPeriod from '@/components/seller/PostPeriod';
-import { postEventState } from '@/states/Events';
 import PostLocation from '@/components/seller/PostLocation';
 import PostCategory from '@/components/seller/PostCategory';
 import PostEventName from '@/components/seller/PostEventName';
 
 export default function PostForm() {
   const navigate = useNavigate();
-  const postEventData = useRecoilValue(postEventState);
+  const eventFormValue = useRecoilValue(eventFormState);
 
   // 폼 제출 핸들러
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    if (
+      !eventFormValue.name.trim() ||
+      !eventFormValue.city ||
+      !eventFormValue.district ||
+      !eventFormValue.startDate ||
+      !eventFormValue.endDate ||
+      !eventFormValue.description ||
+      eventFormValue.category === '카테고리'
+    ) {
+      customToast('모든 항목을 기입해주세요!', 'error');
+      return;
+    }
+
     try {
-      await postEvent(postEventData);
+      await postEvent(eventFormValue);
       customToast('행사가 등록되었습니다!', 'success');
       navigate('/events');
     } catch (error) {
-      customToast('모든 항목을 기입해주세요!', 'error');
+      customToast('등록에 실패했습니다!', 'error');
     }
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="container mx-auto mb-10 flex flex-col gap-4 bg-gray-100 px-10 sm:mb-0 sm:px-40"
+      className="container mx-auto mb-10 flex flex-col gap-4 bg-white px-10 sm:mx-24 sm:mt-16 sm:px-40 sm:py-12"
     >
-      <PostTitle />
+      <Title text={eventData.EVENT_POST_STORE.title} center />
       <PostCategory />
       <PostEventName />
       <PostLocation />
